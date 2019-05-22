0cb365441eec4784951131e6ae11b0e087f1bc62
Henry <hmf4455@rit.edu>
2019-05-22 14:25:19 -0400

    now testing input

--- scripter.c
+++ scripter.c
@@ -6,6 +6,8 @@
 #define STDOUT 1
 #define STDERR 2
 
+#define BUF_SIZE 2048
+
 //int startProcess(char *args, int myIn, int myOut);
 //
 int readline(FILE *stream, char* line);
@@ -20,59 +22,60 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    int fdprocess[2];    // Communications to <process>
+    int fdpout[2];    // Communications to <process>
+    int fdpin[2];
 
     // Create communication pipes
-    pipe(fdprocess);
+    pipe(fdpout);
+    pipe(fdpin);
 
     int pid = fork();
-    if (pid == 0) {
-        dup2(fdprocess[1], STDOUT);   // send stdout to the pipe
-        dup2(fdprocess[1], STDERR);   // send stdout to the pipe
-        close(fdprocess[0]);
+    if (pid == 0) { // the "process"
+        dup2(fdpout[1], STDOUT);    // send stdout to the pipe
+        dup2(fdpout[1], STDERR);    // send stdout to the pipe
+        dup2(STDIN, fdpin[0]);      // send pipe to stdin
+
+        close(fdpout[0]);   // we will not read from the output
+
+        // close(fdpin[1]);    // we will not write to the input pipe
+        
 
         printf("Started process\n");
 
         //char *argpist[] = {"/usr/bin/python3", "womp.py"};
-        char *argpist[] = {argv[1], argv[1]};
+        char *argpist[] = {argv[1], argv[1], (char *) NULL};
         execv(argpist[0], argpist);
 
     }
-    else {
+    else if (0) { // the main scripter process
 
-        char buffer[2048]; // meh?
-        char result[2048];
-        FILE *inputstream = fdopen(fdprocess[0], "r");
-        FILE *outputstream = fdopen(fdprocess[1], "a");
-        close(fdprocess[1]);
+        char buffer[BUF_SIZE]; // meh?
+        char result[BUF_SIZE];
+        FILE *inputstream = fdopen(fdpout[0], "r");
+        FILE *outputstream = fdopen(fdpin[1], "a");
 
-
+        close(fdpout[1]);   // we won't write to the output pipe
+        close(fdpin[0]);    // we won't read from the input pipe
+        
         while (readline(inputstream, buffer) != EOF) { 
 
-            // These lines of code work fine here
-            /* char *arr[] = {"/bin/echo", "from dawn till dust"};
-            int val = execv(arr[0], arr);
-            printf("%d\n", val); */
+            printf("Read a line\n");
 
-
-            char *argHandler[] = {argv[2], argv[2]}; 
-
-            // but placed here they do not work
-            char *arr[] = {"/bin/echo", "from dawn till dust", (char *) NULL};
-            int val = execv(arr[0], arr);
-            printf("%d\n", val);
-
+            char *argHandler[] = {argv[2], argv[2], (char *) NULL}; 
 
             handle(buffer, result, argHandler, pid);
-            printf("RESULT: %s\n", result);
-            fprintf(outputstream, "%s\n", result);
-        }
 
-        // char *argpist[] = {argv[1], argv[1]};
-        // execv(argpist[0], argpist);
+            fprintf(stdout, "RESULT: %s\n", result);
+            fprintf(outputstream, "%s\n", result); // response from handler
+            printf("Message sent. (1)\n");
+            //fflush(outputstream);
+
+            printf("Message sent. (2)\n");
+
+        }
+        printf("Process terminated, good bye\n");
 
         fclose(inputstream);
-        fclose(outputstream);
     }
 }
 
@@ -80,17 +83,13 @@ int main(int argc, char **argv) {
 void handle(char *input, char *result, char *args[], int pid) {
 
     int fdhandler[2];
-    pipe(fdhandler);
+    pipe(fdhandler); // pipe for child process output
        
-    if (fork() == 0) {
-        printf("%s\n", input);
+    if (fork() == 0) { // Child process execution
+        printf("FROM PROCESS: %s\n", input);
         dup2(fdhandler[1], STDOUT); // pipe stdout to the write-in of the pipe
         
         close(fdhandler[0]); // we don't need to write to the pipe in the child
-
-        // Test that STDOUT is redirected to the pipe
-        printf("This should go in the pipe\n");
-        printf("This should go in the pipe too\n");
 
         execv(args[0], args);
         exit(0);
@@ -98,17 +97,10 @@ void handle(char *input, char *result, char *args[], int pid) {
 
     FILE *resultStream = fdopen(fdhandler[0], "r");
     close(fdhandler[1]); // close the write end of the pipe in parent
-                        // this forces the output to flush, otherwise we'd
-                        // have to wait for the pipe to fill up entirely
+                         // this forces the output to flush, otherwise we'd
+                         // have to wait for the pipe to fill up entirely
 
-    //readline(resultStream, result); // Only read one line
-
-    // Using a loop to read arbitrary # of lines now
-    while (readline(resultStream, result) != EOF) { 
-        // reads one line into the result buffer at a time
-
-        printf("blah: %s\n", result);
-    }
+    readline(resultStream, result); // Only read one line
 
     close(fdhandler[0]); // close the read end of the pipe
 }
